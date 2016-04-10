4c8012db8d6f0a6d7d27a1a75a13801f781679ed
jongber <jongber@gmail.com>
2016-04-10 18:54:46 +0900

    Opengl term 1 created

---
+++ app/src/main/java/glproj/jongber/androidopengl/term1/activity/OpenGLActivity.java
@@ -0,0 +1,30 @@
+package glproj.jongber.androidopengl.term1.activity;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import glproj.jongber.androidopengl.term1.view.MyGLSurfaceView;
+
+/**
+ * Created by jongber on 2016-04-10.
+ *
+ * On 'AndroidManifest.xml' file add '<uses-feature android:glEsVersion="0x00020000" android:required="true" />'
+ *
+ * This class and samples comes from 'http://developer.android.com/intl/ko/training/graphics/opengl/environment.html'
+ *
+ */
+public class OpenGLActivity extends Activity
+{
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+}
